ab01d1b25dcf38277177b2119a2ee3e44c03d802
jaroinz <jaroinz@gmail.com>
2019-07-13 19:25:43 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-jgsmxp
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-jgsmxp)
---
+++ src/app/product-list/product-list.component.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+import { products } from '../products';
+
+@Component({
+  selector: 'app-product-list',
+  templateUrl: './product-list.component.html',
+  styleUrls: ['./product-list.component.css']
+})
+export class ProductListComponent {
+  products = products;
+
+  constructor() { }
+
+  share() {
+    window.alert('The product has been shared!');
+  }
+
+  onNotificar(event) {
+    window.alert('You will be notified when the product goes on sale. event. ' + event);
+  }
+}
+
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at http://angular.io/license
+*/
